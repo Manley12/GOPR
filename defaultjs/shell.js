5f7084340d8d5ff1e931b9e6bb9fe5a88ef1de72
Gregory Manley <gregory.f.manley@gmail.com>
2019-02-06 08:04:10 -0500

    Update shell.js
    add some more functions that may have been important

--- defaultjs/shell.js
+++ defaultjs/shell.js
@@ -105,7 +105,6 @@ classShell.prototype.screenMainMenu = function() {
     this.prompt();
 };
         
-
 /*Option 6*/
 classShell.prototype.mopr = function() {
     OS.output.pushBuffer(['AN INTERESTING GAME.']);
@@ -114,4 +113,113 @@ classShell.prototype.mopr = function() {
         this.prompt();
     }).bind(this), 2000);
 };
+
+/*Option 7*/
+classShell.prototype.gtnw = function() {
+    OS.output.pushBuffer([
+        '********* SELECT INITIAL STRATEGY ********',' ',
+        '1. US first strike          21. Nicaraguan thrust ',
+        '2. USSR first strike        22. Greenland domestic ',
+        '3. NATO / Warsaw Pact       23. Iceland heavy ',
+        '4. Far East strategy        24. Kenya option ',
+        '5. US USSR escalation       25. Pacific defense',
+        '6. Middle East war          26. Uganda maximum ',
+        '7. USSR - China attack      27. Thai subversion ',
+        '8. India Pakistan war       28. Romanian strike ',
+        '9. Mediterranean war        29. Pakistan sovereignty ',
+        '10. Hongkong variant        30. Afghan misdirection',
+        '11. SEATO decapitating      31. Thai variation ',
+        '12. Cuban provocation       32. Northern territorial ',
+        '13. Inadvertent             33. Polish paramilitary ',
+        '14. Atlantic heavy          34. S.African offensive ',
+        '15. Cuban paramilitary      35. Panama misdirection',
+        '16. Nicaraguan preemptive   36. Scandinavian domestic ',
+        '17. Pacific territorial     37. Jordan preemptive ',
+        '18. Burmese theatrewide     38. English thrust ',
+        '19. Turkish decoy           39. Burmese maneuver ',
+        '20. NATO recursion          40. Spain counter',
+        ' '
+    ]);
+    this.prompt();
+};
+
+classShell.prototype.comHelp = function() {
+    
+    var page1 = [
+        ' ',
+        " AVAILABLE COMMANDS:",
+        ' ',
+        ' HELP - show this screen ',
+        ' WELCOME - show welcome screen',
+        ' '
+    ];
+    
+    OS.outputpushBuffer( page1 );
+    this.prompt();
+    
+};
+
+classShell.prototype.comContact = function() {
+    OS.outputBuffer([
+        'D.O.D PENSION FILES INDICATE CURRENT MAILING AS: ',
+        ' ',
+        ' '
+    ]);
+    this.prompt();
+};
+
+classShell.prototype.screenDisconnet = function() {
+    OS.output.pushBuffer('CPE 1704 TKS ');
+    for (var i=0;i<10;i++) {
+        OS.output.pushBuffer(String.fromCharCode(Math.ceil(127*Math.random())));
+    }
+    OS.output.pushBuffer('NO CARRIER ');
+    OS.output = null;
+};
+
+classShell.prototype.comInterpreter = function( args ) {
+    try {
+        // 1. Parse arguments
+        var filename = args[1].toLowerCase();
+        var extension = filename.split(/\./).pop();
+        
+        // 2. Load source code
+        $.get('/code/'+filename, (function(extension,source) {
+            // 3. Execute
+            switch(extension) {
+                case 'bas':
+                    var BASIC = new classBASIC( OS );
+                    BASIC.run( source, this.prompt.bind(this) );
+                    break;
+                case 'js':
+                    OS.output.pushBuffer('JAVASCRIPT NOT RECOGNIZED (yet :)\n');
+                    this.prompt();
+                    break;
+                default:
+                    OS.output.pushBuffer('FILETYPE NOT RECOGNIZED.\n');
+                    this.prompt();
+            }
+        }).bind(this,extension)
+        ).fail( this.prompt.bind(this) );
             
+    } catch (e) {
+        this.prompt();
+    }
+};
+
+classShell.prototype.comView = function( args ) {
+    try {
+        // 1. Parse arguments
+        var filename = args[1].toLowerCase();
+        var extension = filename.split(/\./).pop();
+        
+        // 2. Load source code
+        $.get('/code/'+filename, (function(extension,source) {
+            OS.output.pushBuffer("\n\n" + source + "\n\n");
+            this.prompt();
+        }).bind(this,extension));
+
+    } catch (e) {
+        this.prompt();
+    }
+};
